d7fc1f6008173cbe98af69ad6cb96b24271450be
jasonwynn10 <jasonwynn10@gmail.com>
2017-10-07 19:10:09 -0400

    Completed Crates Plugin

--- Crates/src/jasonwynn10/Crates/Main.php
+++ Crates/src/jasonwynn10/Crates/Main.php
@@ -1,9 +1,101 @@
 <?php
 namespace jasonwynn10\Crates;
 
+use pocketmine\block\Block;
+use pocketmine\block\Chest;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\inventory\InventoryCloseEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\nbt\JsonNBTParser;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener {
-	//
+	public function onEnable() {
+		@mkdir($this->getDataFolder());
+		new Config($this->getDataFolder()."config.yml",Config::YAML, [
+			"Spirit" => [
+				"stick 2 {display:{Name:\"§r§6§lNormal stick\"}}",
+				"diamond_sword 1",
+				"wool:4 3"
+			],
+			"Paranormal" => ["stick 1 {ench:[{id:12s,lvl:10s}]}"],
+			"Psycho" => ["stick 1 {display:{Name:\"§eTHE POWER\"},ench:[{id:12s,lvl:50s}]}"],
+			"Reaper" => ["stick 1 {display:{Name:\"§eTHE POWER\"},ench:[{id:12s,lvl:50s}]}"],
+			"DemonLord" => ["stick 1 {display:{Name:\"§eTHE POWER\"},ench:[{id:12s,lvl:50s}]}"]
+		]);
+		$this->getConfig()->reload();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onPlace(BlockPlaceEvent $event) {
+		if($event->getBlock() instanceof Chest) {
+			$this->getServer()->getScheduler()->scheduleDelayedTask(new class($this, $event->getBlock()->asPosition()) extends PluginTask{
+				public function __construct(Plugin $owner, Position $coords) {
+					parent::__construct($owner);
+					$this->coords = $coords;
+				}
+				public function onRun(int $currentTick) {
+					/** @var \pocketmine\tile\Chest|null $tile */
+					$tile = $this->coords->getLevel()->getTile($this->coords);
+					if(in_array($tile->getName(), $this->getOwner()->getConfig()->getAll(true))) {
+						$tile->namedtag->Lock = new StringTag("Lock", $tile->getName());
+					}
+				}
+			}, 1); // 1 tick delay
+		}
+	}
+	public function onTap(PlayerInteractEvent $ev) {
+		$block = $ev->getBlock();
+		$hand = $ev->getItem();
+		if($block instanceof Chest) {
+			/** @var \pocketmine\tile\Chest|null $chestTile */
+			$chestTile = $block->getLevel()->getTile($block);
+			if($chestTile !== null and in_array($chestTile->getName(), $this->getConfig()->getAll(true))) {
+				if($hand->getId() !== Block::TRIPWIRE_HOOK) {
+					$chestTile->getInventory()->clearAll();
+					$ev->setCancelled();
+					return;
+				}
+				$items = [];
+				foreach($this->getConfig()->get($chestTile->getName(), []) as $itemString) {
+					$arr = explode(" ", $itemString);
+					$item = Item::fromString($arr[0]);
+					if(!isset($arr[1])) {
+						$item->setCount($item->getMaxStackSize());
+					}else{
+						$item->setCount((int) $arr[1]);
+					}
+					if(isset($arr[2])) {
+						$tags = $exception = null;
+						$data = implode(" ", array_slice($arr, 2));
+						try{
+							$tags = JsonNBTParser::parseJSON($data);
+						}catch(\Throwable $ex) {
+							$exception = $ex;
+						}
+						if(!$tags instanceof CompoundTag or $exception !== null) {
+							$this->getLogger()->error("Invalid NBT tag!");
+							return;
+						}
+						$item->setNamedTag($tags);
+					}
+					$items[] = $item;
+				}
+				$chestTile->getInventory()->setContents($items, true);
+			}
+		}
+	}
+	public function onInventoryClose(InventoryCloseEvent $ev) {
+		$chestTile = $ev->getInventory()->getHolder();
+		if($chestTile instanceof \pocketmine\tile\Chest and in_array($chestTile->getName(), $this->getConfig()->getAll(true))) {
+			$ev->getPlayer()->getInventory()->getItemInHand()->pop(); // TODO: find a better method because this creates a vulnerability to keep the crate key
+		}
+	}
 }
