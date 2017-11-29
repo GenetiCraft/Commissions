31189dd5b9a2abd32615b8aee38585a166cbdaa5
jasonwynn10 <jasonwynn10@gmail.com>
2017-11-29 16:50:11 -0500

    Finished SQLite3 Provider

--- CR/src/jasonwynn10/CR/provider/KingdomProvider.php
+++ CR/src/jasonwynn10/CR/provider/KingdomProvider.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 namespace jasonwynn10\CR\provider;
 
 use jasonwynn10\CR\Main;
+use pocketmine\IPlayer;
 
 abstract class KingdomProvider {
+	protected $plugin;
+
 	/**
 	 * KingdomProvider constructor.
 	 *
 	 * @param Main $plugin
 	 */
-	abstract public function __construct(Main $plugin);
+	public function __construct(Main $plugin) {
+		$this->plugin = $plugin;
+	}
 
 	/**
 	 * @param string $kingdom
@@ -18,6 +23,12 @@ abstract class KingdomProvider {
 	 * @return string
 	 */
 	abstract public function getKingdomLeader(string $kingdom) : string;
+
+	/**
+	 * @param string  $kingdom
+	 * @param IPlayer $player
+	 */
+	abstract public function setKingdomLeader(string $kingdom, IPlayer $player);
 
 	/**
 	 * @param string $kingdom
@@ -30,9 +41,31 @@ abstract class KingdomProvider {
 	 * @param string $kingdom
 	 * @param int    $power
 	 *
-	 * @return mixed
+	 * @return bool
 	 */
-	abstract public function setKingdomPower(string $kingdom, int $power);
+	abstract public function setKingdomPower(string $kingdom, int $power) : bool;
+
+	/**
+	 * @param string $kingdom
+	 * @param int    $power
+	 *
+	 * @return bool
+	 */
+	public function addKingdomPower(string $kingdom, int $power) : bool {
+		$current = $this->getKingdomPower($kingdom);
+		return $this->setKingdomPower($kingdom, $current + abs($power));
+	}
+
+	/**
+	 * @param string $kingdom
+	 * @param int    $power
+	 *
+	 * @return bool
+	 */
+	public function removeKingdomPower(string $kingdom, int $power) : bool {
+		$current = $this->getKingdomPower($kingdom);
+		return $this->setKingdomPower($kingdom, $current - abs($power));
+	}
 
 	/**
 	 * @param string $kingdom
@@ -43,8 +76,74 @@ abstract class KingdomProvider {
 
 	/**
 	 * @param string $kingdom
+	 * @param float  $money
+	 *
+	 * @return bool
+	 */
+	abstract public function setKingdomMoney(string $kingdom, float $money) : bool;
+
+	/**
+	 * @param string $kingdom
+	 * @param float  $money
+	 *
+	 * @return bool
+	 */
+	public function addKingdomMoney(string $kingdom, float $money) : bool {
+		$current = $this->getKingdomMoney($kingdom);
+		return $this->setKingdomMoney($kingdom, $current + abs($money));
+	}
+
+	/**
+	 * @param string $kingdom
+	 * @param float  $money
+	 *
+	 * @return bool
+	 */
+	public function removeKingdomMoney(string $kingdom, float $money) : bool {
+		$current = $this->getKingdomMoney($kingdom);
+		return $this->setKingdomMoney($kingdom, $current - abs($money));
+	}
+
+	/**
+	 * @param string $kingdom
 	 *
 	 * @return array
 	 */
 	abstract public function getKingdomMembers(string $kingdom) : array;
+
+	/**
+	 * @param string $kingdom
+	 * @param array  $members
+	 *
+	 * @return bool
+	 */
+	abstract public function setKingdomMembers(string $kingdom, array $members) : bool;
+
+	/**
+	 * @param string  $kingdom
+	 * @param IPlayer $player
+	 *
+	 * @return bool
+	 */
+	public function addKingdomMember(string $kingdom, IPlayer $player) : bool {
+		$current = $this->getKingdomMembers($kingdom);
+		array_push($current, $player->getName());
+		return $this->setKingdomMembers($kingdom, $current);
+	}
+
+	/**
+	 * @param string  $kingdom
+	 * @param IPlayer $member
+	 *
+	 * @return bool
+	 */
+	public function removeKingdomMember(string $kingdom, IPlayer $member) : bool {
+		$current = $this->getKingdomMembers($kingdom);
+		$key = array_search($member->getName(), $current);
+		if($key !== false) {
+			unset($current[$key]);
+			return $this->setKingdomMembers($kingdom, $current);
+		}
+		return false;
+	}
 }
--- CR/src/jasonwynn10/CR/provider/SQLite3Provider.php
+++ CR/src/jasonwynn10/CR/provider/SQLite3Provider.php
@@ -2,6 +2,126 @@
 declare(strict_types=1);
 namespace jasonwynn10\CR\provider;
 
+use jasonwynn10\CR\Main;
+use pocketmine\IPlayer;
+
 class SQLite3Provider extends KingdomProvider {
-	//
+	/** @var \SQLite3 $db */
+	private $db;
+	/** @var \SQLite3Stmt  */
+	private $sqlGetKingdom, $sqlSetKingdomLeader, $sqlSetKingdomPower, $sqlSetKingdomMoney, $sqlSetKingdomMembers;
+
+	/**
+	 * SQLite3Provider constructor.
+	 *
+	 * @param Main $plugin
+	 */
+	public function __construct(Main $plugin) {
+		parent::__construct($plugin);
+		$db = $this->db = new \SQLite3($plugin->getDataFolder()."kingdoms.db");
+		$db->exec("CREATE TABLE IF NOT EXISTS kingdoms (kingdom TEXT PRIMARY KEY, leader TEXT, power INTEGER, money FLOAT, members TEXT);");
+		$this->sqlGetKingdom = $db->prepare("SELECT * FROM kingdoms WHERE kingdom = :kingdom;");
+		$this->sqlSetKingdomLeader = $db->prepare("UPDATE kingdoms SET leader = :leader WHERE kingdom = :kingdom;");
+		$this->sqlSetKingdomPower = $db->prepare("UPDATE kingdoms SET power = :power WHERE kingdom = :kingdom;");
+		$this->sqlSetKingdomMoney = $db->prepare("UPDATE kingdoms SET money = :money WHERE kingdom = :kingdom;");
+		$this->sqlSetKingdomMembers = $db->prepare("UPDATE kingdoms SET memebrs = :members WHERE kingdom = :kingdom;");
+	}
+
+	/**
+	 * @param string $kingdom
+	 *
+	 * @return string
+	 */
+	public function getKingdomLeader(string $kingdom) : string {
+		$this->sqlGetKingdom->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$result = $this->sqlGetKingdom->execute();
+		/** @noinspection PhpIncompatibleReturnTypeInspection */
+		return $result !== false ? $result->fetchArray(SQLITE3_ASSOC)["leader"] : "";
+	}
+
+	/**
+	 * @param string  $kingdom
+	 * @param IPlayer $player
+	 *
+	 * @return bool
+	 */
+	public function setKingdomLeader(string $kingdom, IPlayer $player) : bool{
+		$this->sqlSetKingdomLeader->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$this->sqlSetKingdomLeader->bindValue(":leader", $player->getName(), SQLITE3_TEXT);
+		return $this->sqlSetKingdomLeader->execute() !== false;
+	}
+
+	/**
+	 * @param string $kingdom
+	 *
+	 * @return int
+	 */
+	public function getKingdomPower(string $kingdom) : int{
+		$this->sqlGetKingdom->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$result = $this->sqlGetKingdom->execute();
+		/** @noinspection PhpIncompatibleReturnTypeInspection */
+		return $result ? $result->fetchArray(SQLITE3_ASSOC)["power"] : 0;
+	}
+
+	/**
+	 * @param string $kingdom
+	 * @param int    $power
+	 *
+	 * @return bool
+	 */
+	public function setKingdomPower(string $kingdom, int $power) : bool {
+		$this->sqlSetKingdomPower->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$this->sqlSetKingdomPower->bindValue(":power", $power < 0 ? 0 : $power, SQLITE3_INTEGER);
+		return $this->sqlSetKingdomPower->execute() !== false;
+	}
+
+	/**
+	 * @param string $kingdom
+	 *
+	 * @return float
+	 */
+	public function getKingdomMoney(string $kingdom) : float {
+		$this->sqlGetKingdom->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$result = $this->sqlGetKingdom->execute();
+		/** @noinspection PhpIncompatibleReturnTypeInspection */
+		return $result !== false ? $result->fetchArray(SQLITE3_ASSOC)["money"] : 0.00;
+	}
+
+	/**
+	 * @param string $kingdom
+	 * @param float  $money
+	 *
+	 * @return bool
+	 */
+	public function setKingdomMoney(string $kingdom, float $money) : bool {
+		$this->sqlSetKingdomMoney->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$this->sqlSetKingdomMoney->bindValue(":power", $money < 0 ? 0.00 : $money, SQLITE3_FLOAT);
+		return $this->sqlSetKingdomMoney->execute() !== false;
+	}
+
+	/**
+	 * @param string $kingdom
+	 *
+	 * @return array
+	 */
+	public function getKingdomMembers(string $kingdom) : array {
+		$this->sqlGetKingdom->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$result = $this->sqlGetKingdom->execute();
+		if($result !== false) {
+			return explode(",", $result->fetchArray(SQLITE3_ASSOC)["members"]);
+		}
+		return [];
+	}
+
+	/**
+	 * @param string $kingdom
+	 * @param array  $members
+	 *
+	 * @return bool
+	 */
+	public function setKingdomMembers(string $kingdom, array $members) : bool {
+		$this->sqlSetKingdomMembers->bindValue(":kingdom", $kingdom, SQLITE3_TEXT);
+		$this->sqlSetKingdomMembers->bindValue(":members", implode(",", $members), SQLITE3_TEXT);
+		return $this->sqlSetKingdomMembers->execute() !== false;
+	}
 }
